a929d171e79a614303ea6d48d7f7cc0827eb8873
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-25 07:27:40 +0700

    Widget Text

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Aplikasi Hello World')),
-        body: Center(child: Text("Hello World")),
+        body: Center(
+          child: Container(
+            color: Colors.lightBlue,
+            width: 150,
+            height: 50,
+            child: Text(
+              "Saya sedang melatih kemampuan flutter saya",
+              maxLines: 2,
+            ),
+          ),
+        ),
       ),
     );
   }
